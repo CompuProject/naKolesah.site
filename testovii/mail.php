90ab593539f9173dd0caf74bab07e065e75519fe
Unknown <alexmelkov@mail.ru>
2019-01-20 15:49:19 +0300

    Правки по Дискам

--- testovii/mail.php
+++ testovii/mail.php
@@ -22,7 +22,22 @@ switch ($post_data['type']) {
         addFormDataToTheDatabase('ПРОВЕРИТЬ НАЛИЧИЕ ДИСКОВ ДЛЯ МОЕГО АВТО',$post_data['phone'],$post_data['urlName']);
         echo json_encode(['callback' => true]);
         break;
+    case 'proverka-nalichiya-form-mobile':
+        $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПРОВЕРИТЬ НАЛИЧИЕ ДИСКОВ ДЛЯ МОЕГО АВТО'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p><br>';
+        $message .= '<p>Бренд: <b>'.$post_data['brand'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ПРОВЕРИТЬ НАЛИЧИЕ ДИСКОВ ДЛЯ МОЕГО АВТО',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
     case 'podbor-modal-form':
+        $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПОДОБРАТЬ ДИСКИ'";
+        $message = '<p>Бренд: <b>'.$post_data['brand'].'</b></p><p>Диаметр: <b>'.$post_data['diameter'].'</b></p><p>Цена: <b>'.$post_data['price'].'</b></p><p>Номер телефона: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ПОДОБРАТЬ ДИСКИ',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'podbor-modal-form-mobile':
         $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПОДОБРАТЬ ДИСКИ'";
         $message = '<p>Бренд: <b>'.$post_data['brand'].'</b></p><p>Диаметр: <b>'.$post_data['diameter'].'</b></p><p>Цена: <b>'.$post_data['price'].'</b></p><p>Номер телефона: <b>'.$post_data['phone'].'</b></p>';
         mail($to, $subject, $message, $headers);
@@ -36,6 +51,13 @@ switch ($post_data['type']) {
         addFormDataToTheDatabase('ПРИОБРЕСТИ СТИЛЬНЫЕ ДИСКИ В РАССРОЧКУ',$post_data['phone'],$post_data['urlName']);
         echo json_encode(['callback' => true]);
         break;
+    case 'priobresti-rassrochka-form-mobile':
+        $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПРИОБРЕСТИ СТИЛЬНЫЕ ДИСКИ В РАССРОЧКУ'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ПРИОБРЕСТИ СТИЛЬНЫЕ ДИСКИ В РАССРОЧКУ',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
     case 'onlain-podbor-diskov-form':
         $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПОСМОТРЕТЬ ЛУЧШИЕ ДИСКИ ДЛЯ МОЕГО АВТО'";
         $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
@@ -43,7 +65,21 @@ switch ($post_data['type']) {
         addFormDataToTheDatabase('ПОСМОТРЕТЬ ЛУЧШИЕ ДИСКИ ДЛЯ МОЕГО АВТО',$post_data['phone'],$post_data['urlName']);
         echo json_encode(['callback' => true]);
         break;
+    case 'onlain-podbor-diskov-form-mobile':
+        $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПОСМОТРЕТЬ ЛУЧШИЕ ДИСКИ ДЛЯ МОЕГО АВТО'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ПОСМОТРЕТЬ ЛУЧШИЕ ДИСКИ ДЛЯ МОЕГО АВТО',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
     case 'callback-form-footer':
+        $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПЕРЕЗВОНИТЕ МНЕ ФУТЕР'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ПЕРЕЗВОНИТЕ МНЕ ФУТЕР',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'callback-form-footer-mobile':
         $subject = "\"ДИСКИ\" Сообщение с Лендинга - 'ПЕРЕЗВОНИТЕ МНЕ ФУТЕР'";
         $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
         mail($to, $subject, $message, $headers);
