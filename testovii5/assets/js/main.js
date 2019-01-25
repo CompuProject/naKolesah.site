f6bacbe449c059f48d7e811d3ace4dbae39d5332
Paul <pozhidaevpro@gmail.com>
2019-01-25 13:52:40 +0300

    Тест/хранение шин

---
+++ testovii5/assets/js/main.js
@@ -0,0 +1,138 @@
+
+$(document).ready(function () {
+    if (siteUrl == "zamena-masel") {
+        $(".changePhone").html("8 (800) 550-85-93");
+    } else if (siteUrl == "zamena-masla") {
+        $(".changePhone").html("8 (800) 550-85-73");
+    }
+    //маска телефона для input popup
+    $('input[name=phone]').click(function () {
+        $(this).focus();
+    });
+    $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+    //обработка кнопки вызова modal
+    function getModal() {
+        $('.btnModal').click(function (e) {
+            e.preventDefault();
+            var modalId = $(this).attr('id');
+            $('.form1-cover[data-mod=' + modalId + ']').show();
+            $('.form1-cover[data-mod=' + modalId + ']').closest('.modal').show();
+            if (modalId == "modal6") {
+                $('.modalOverlay').show();
+            }
+        });
+    };
+    getModal();
+
+    //обработка форм
+    function sendModal() {
+        $('.form1-cover .modalSubmit').click(function (e) {
+            e.preventDefault();
+            var parentForm = $(this).closest('.form1-cover');
+            var phone = parentForm.find('input[name=phone]').val();
+            phone = phone.replace(/\s+/g,'');
+            var modalName = parentForm.attr('id');
+            var brand = parentForm.find('select[name=brand] option:selected').text();
+            brand = brand.replace(/\s+/g,'');
+            var diameter = parentForm.find('select[name=diameter] option:selected').text();
+            diameter = diameter.replace(/\s+/g,'');
+            var year = parentForm.find('select[name=year] option:selected').text();
+            year = year.replace(/\s+/g,'');
+            var price = parentForm.find('.irs-single').text();
+            price = price.replace(/\s+/g,'');
+            $('.errMsg').hide();
+
+            if (phone == '') {
+                parentForm.find('input[name=phone]').after('<div class="errMsg">Не заполнено поле</div>');
+                parentForm.find('input[name=phone]').css('border-color', 'red');
+            } else {
+                console.log('type=' + modalName + '&phone=' + phone + '&brand=' + brand + '&diameter=' + diameter + '&year=' + year + '&price=' + price + '&urlName=' + location.href.replace('http://', ''));
+
+                $.ajax({
+                    url: '/testovii4/mail.php',
+                    type: 'POST',
+                    data: 'type=' + modalName + '&phone=' + phone + '&brand=' + brand + '&diameter=' + diameter + '&year=' + year+ '&price=' + price + '&urlName=' + location.href.replace('http://', ''),
+                    // data: data,
+                    // dataType: 'html',
+                    success: function (rezult) {
+
+
+                        if (!parentForm.hasClass('nohidemodal')) {
+                            parentForm.hide();
+                            $('.modal').hide();
+                        } else {
+                            $('.modalOverlay').show();
+                        };
+
+                        $('.successMsg').show().delay(3000).fadeOut();
+                        $('.modal-backdrop.fade.in').delay(3000).fadeOut();
+                        $('.modalOverlay').delay(3000).fadeOut();
+                        $('.successMsg,.modal-backdrop.fade.in,.modalOverlay').click(function () {
+                            $('.successMsg,.modal-backdrop.fade.in,.modalOverlay').hide();
+                        });
+
+                    }
+                });
+            }
+        });
+    }
+
+    sendModal();
+
+    function generateHash() {
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
+
+    function getWilgood() {
+        $('.form1-cover .modalSubmit').click(function (event) {
+            // event.preventDefault();
+            var form = $(this).closest('.form1-cover'),
+                COMMENT = form.attr('id');
+            var phone = form.find('input[name=phone]').val();
+            phone = phone.replace(/\s+/g,'');
+            if (phone !== '') {
+                $.ajax({
+                    url: 'https://wilgood.ru/handler_for_partners/',
+                    type: 'GET',
+                    data: 'type_partner=generatorprodaj&comment=' + COMMENT + '&type_response=html&phone=' + phone + '&unique_code=' + generateHash() + '&hash=Agf0FDw6gkRuqsfOQB7cqK9k60qD17f',
+                }).done(json => {
+                    if (json) {
+                        console.log(json);
+                    }
+                });
+            }
+        });
+    }
+
+    getWilgood();
+
+    var fakeRoistatWidth = ($(window).width() - $('.fakeRoistat').width()) / 2;
+    $('.fakeRoistat').css('left', fakeRoistatWidth);
+
+    setTimeout(function () {
+        if (!$('.fakeRoistat').hasClass('check')) {
+            $('.fakeRoistat').slideDown(500);
+            $('.modalOverlay').show();
+        }
+    }, 20000);
+
+    $('.fakeRoistat .close').click(function () {
+        $('body').css('overflow', 'auto');
+        $('.fakeRoistat').hide();
+        $('.modalOverlay').hide();
+    });
+    $('.modalOverlay').click(function () {
+        $('body').css('overflow', 'auto');
+        $('.fakeRoistat').hide();
+        $('.modalOverlay').hide();
+    });
+
+    $(document).mouseleave(function () {
+        if (!$('.fakeRoistat').hasClass('check')) {
+            $('.fakeRoistat').slideDown(500);
+            $('.modalOverlay').show();
+            $('.fakeRoistat').addClass('check');
+        }
+    });
+});
---
+++ testovii5/mail.php
@@ -0,0 +1,85 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . "/lib/function/form_function_init.php";
+$post_data = $_POST;
+$to = 'sales-kolesa@yandex.ru, order@salesgenerator.pro';
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html\r\n";
+$headers .= 'From: <hranenie-shin@sales-kolesa.site>' . "\r\n";
+var_dump($_POST);
+switch ($post_data['type']) {
+    case 'callback-top':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'Форма заказа обратного звонка'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('Форма заказа обратного звонка',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'get-tyres':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'ПРОВЕРИТЬ НАЛИЧИЕ МЕСТ НА СКЛАДЕ'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ПРОВЕРИТЬ НАЛИЧИЕ МЕСТ НА СКЛАДЕ',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'left-widget-get-tyres':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'РАССЧИТАТЬ СТОИМОСТЬ'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p><p>Диаметр диска: <b>'.$post_data['diametr'].'</b></p><p>Хранение с: <b>'.$post_data['disk'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('РАССЧИТАТЬ СТОИМОСТЬ',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'second-get-tyres-form':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'СДАТЬ ШИНЫ В БЛИЖАЙШИЙ АВТОЦЕНТР'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('СДАТЬ ШИНЫ В БЛИЖАЙШИЙ АВТОЦЕНТР',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'bronirovanie':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'ЗАБРОНИРОВАТЬ МЕСТО НА СКЛАДЕ'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ЗАБРОНИРОВАТЬ МЕСТО НА СКЛАДЕ',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'hranenie-shin':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'ОСТАВИТЬ ЗАЯВКУ НА ХРАНЕНИЕ'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ОСТАВИТЬ ЗАЯВКУ НА ХРАНЕНИЕ',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'store-amount':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'ЗАПИСАТЬСЯ НА ШИНОМОНТАЖ В БЛИЖАЙШИЙ АВТОЦЕНТР'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('ЗАПИСАТЬСЯ НА ШИНОМОНТАЖ В БЛИЖАЙШИЙ АВТОЦЕНТР',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'callback-bottom':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'Заказ обратного звонка из футера'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('Заказ обратного звонка из футера',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'order':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'Забронировать место на складе'";
+        $message = '<p>Имя клиента: <b>'.$post_data['name'].'</b></p><p>Электронная почта клиента: <b>'.$post_data['email'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('Забронировать место на складе',$post_data['phone'],$post_data['urlName']);
+        echo json_encode(['callback' => true]);
+        break;
+    case 'zabronirovat_form':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'Забронировать место на складе'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('Забронировать место на складе',$post_data['phone'],$post_data['urlName']);
+        break;
+    case 'modal7':
+        $subject = "\"ХРАНЕНИЕ_ШИН\" Сообщение с Лендинга - 'Lovec'";
+        $message = '<p>Номер телефона клиента: <b>'.$post_data['phone'].'</b></p>';
+        mail($to, $subject, $message, $headers);
+        addFormDataToTheDatabase('Lovec',$post_data['phone'],$post_data['urlName']);
+        break;
+}
